fd2fff177bdcc143deb0a1320d6cbce0669173e4
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2020-01-08 14:22:28 -0500

    initial database setup

---
+++ mood-tracker/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Header = (props) => {
+
+  return (
+    <div>
+      <h1>{props.name}'s Mood Tracker</h1>
+    </div>
+  )
+}
+
+export default Header
